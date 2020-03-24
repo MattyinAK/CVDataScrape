ec7afd8ee3a7ba283113c24559fba674cf7a5dec
MattyinAK <childs.matt@gmail.com>
2020-03-24 08:46:17 -0500

    Initial script version

---
+++ CVData.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from datetime import date
+
+#where our data is located
+URL = 'https://www.worldometers.info/coronavirus/#countries'
+
+#pulled up the URL and inspected the page to find the right info for the table
+#this will grab the table we are after as a list
+df_in_list = pd.read_html(URL, attrs = {'id': 'main_table_countries_today'})
+
+#convert list to dataframe
+df = df_in_list[0]
+
+#replace NaN with zeros
+df = df.fillna(0)
+
+#get today's date
+today = date.today()
+
+#format the date so it can be used as part of the file name
+filedate = today.strftime("%m%d%Y")
+
+#build out the path - make changes here for customization
+path = 'C:\export\CV'+ filedate + '.xlsx'
+
+df.to_excel(path)
